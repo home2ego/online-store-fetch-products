b05984d79338a998e477608534fd6596e2a44865
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-10 18:06:30 +0200

    initial project

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+function App() {
+  return 'Hello World';
+}
+
+createRoot(document.querySelector('#root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

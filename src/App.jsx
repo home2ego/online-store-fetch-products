11ae88857ef911a546bd4ad9a05ab323afcebbf0
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-10 18:14:56 +0200

    add App Component

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  if (loggedIn) {
+    return (
+      <>
+        {/* <StoreFront /> */}
+        <button className="btn btn-outline" onClick={() => setLoggedIn(false)}>
+          Logout
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2>Please login</h2>
+        <button className="btn btn-primary" onClick={() => setLoggedIn(true)}>
+          Login
+        </button>
+      </>
+    );
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 import './index.css';
-
-function App() {
-  return 'Hello World';
-}
 
 createRoot(document.querySelector('#root')).render(
   <StrictMode>

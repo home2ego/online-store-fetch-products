3386b14805ebf43ac4268a4431463e572b3b7be1
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-11 17:20:48 +0200

    refactor StoreFront Component

--- src/StoreFront.jsx
+++ src/StoreFront.jsx
@@ -1,26 +1,24 @@
 import { useEffect, useState } from 'react';
 import Product from './Product.jsx';
 import Loader from './Loader.jsx';
+import useFetch from './useFetch.jsx';
 
 export default function StoreFront() {
   const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { get, loading } = useFetch('https://react-tutorial-demo.firebaseio.com/');
 
   useEffect(() => {
-    fetch('https://react-tutorial-demo.firebaseio.com/products.json')
-      .then((response) => response.json())
+    get('products.json')
       .then((data) => {
+        console.log(data);
         setProducts(data);
       })
-      .catch((error) => console.log(error))
-      .finally(() => {
-        setIsLoading(false);
-      });
+      .catch((error) => console.error(error));
   }, []);
 
   return (
     <div className="store-front">
-      {isLoading && <Loader />}
+      {loading && <Loader />}
       {products.map((product) => (
         <Product key={product.id} details={product} />
       ))}
